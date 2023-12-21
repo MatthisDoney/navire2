499736b4c16efaafe737554b81b10c99de826b4c
MatthisDoney <matthis.doney@outlook.fr>
2023-12-21 11:15:00 +0100

    debut page 4

--- src/Controller/AisShipTypeController.php
+++ src/Controller/AisShipTypeController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use \App\Repository\AisShipTypeRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 #[Route('/aisshiptype', name: 'aisshiptype')]
 class AisShipTypeController extends AbstractController {
@@ -16,5 +18,15 @@ class AisShipTypeController extends AbstractController {
         return $this->render('aisshiptype/voirtous.html.twig', [
             'aisShipTypes' => $aisSipTypes,
         ]);
+        
+    }
+    
+    #[Route('/portscompatibles', name: 'portscompatibles')]
+    public function portsCompatibles(Request $request, AisShipTypeRepository $repo) : Response  {
+        $aisShipType = $repo->find($request->get('id'));
+        return $this->render('aisshiptype/portscompatibles.html.twig'), [
+            'aisShipType' => $aisShipType,
+            ]);
+        
     }
 }
