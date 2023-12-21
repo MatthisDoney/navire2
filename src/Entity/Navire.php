f174c5a8ce9e234564a2353ebfee6e9b593f2476
MatthisDoney <matthis.doney@outlook.fr>
2023-12-21 10:13:53 +0100

    oui

---
+++ src/Entity/Navire.php
@@ -0,0 +1,243 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NavireRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: NavireRepository::class)]
+#[ORM\Index(name:'ind_IMO', columns: ['imo'])]
+#[ORM\Index(name:'ind_MMSI', columns: ['mmsi'])]
+class Navire
+{
+    #[Assert\Unique(fields:['imo', 'mmsi', 'indicatifAppel'])]
+    
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    
+    #[ORM\Column(name:'imo', length: 7)]
+    #[Assert\Regex('[1-9][0-9]{6}', message:'Le numero IMO doit être unique et composé de 7 chiffre sans commencer par zero')]
+    private ?string $imo = null;
+
+    #[ORM\Column(length: 60)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 9)]
+    private ?string $mmsi = null;
+
+    #[ORM\Column(length: 10)]
+    private ?string $indicatifAppel = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $eta = null;
+
+    #[ORM\Column]
+    #[Assert\Range(
+            min: 1,
+            notInRangeMessage: 'La longueur doit etre positive',
+    )]
+    private ?int $longueur = null;
+
+    #[ORM\Column]
+    #[Assert\Range(
+            min: 1,
+            notInRangeMessage: 'La largeur doit etre positive',
+    )]
+    private ?int $largeur = null;
+
+    #[ORM\Column]
+    private ?float $tirantdeau = null;
+
+    #[ORM\ManyToOne(inversedBy: 'navires')]
+    #[ORM\JoinColumn(name:'idaisshiptype', referencedColumnName:'id', nullable: false)]
+    private ?AisShipType $aisShipType = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(name:'idpays', referencedColumnName:'id', nullable: false)]
+    private ?Pays $pavillon = null;
+
+    #[ORM\ManyToOne(inversedBy: 'navires', cascade:['persist'])]
+    #[ORM\JoinColumn(name:'idport', referencedColumnName:'id',nullable:true)]
+    private ?Port $destination = null;
+
+    #[ORM\OneToMany(mappedBy: 'navire', targetEntity: Escale::class, orphanRemoval: true)]
+    private Collection $escales;
+
+    public function __construct()
+    {
+        $this->escales = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getImo(): ?string
+    {
+        return $this->imo;
+    }
+
+    public function setImo(string $imo): static
+    {
+        $this->imo = $imo;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getMmsi(): ?string
+    {
+        return $this->mmsi;
+    }
+
+    public function setMmsi(string $mmsi): static
+    {
+        $this->mmsi = $mmsi;
+
+        return $this;
+    }
+
+    public function getIndicatifAppel(): ?string
+    {
+        return $this->indicatifAppel;
+    }
+
+    public function setIndicatifAppel(string $indicatifAppel): static
+    {
+        $this->indicatifAppel = $indicatifAppel;
+
+        return $this;
+    }
+
+    public function getEta(): ?\DateTimeInterface
+    {
+        return $this->eta;
+    }
+
+    public function setEta(?\DateTimeInterface $eta): static
+    {
+        $this->eta = $eta;
+
+        return $this;
+    }
+
+    public function getLongueur(): ?int
+    {
+        return $this->longueur;
+    }
+
+    public function setLongueur(int $longueur): static
+    {
+        $this->longueur = $longueur;
+
+        return $this;
+    }
+
+    public function getLargeur(): ?int
+    {
+        return $this->largeur;
+    }
+
+    public function setLargeur(int $largeur): static
+    {
+        $this->largeur = $largeur;
+
+        return $this;
+    }
+
+    public function getTirantdeau(): ?float
+    {
+        return $this->tirantdeau;
+    }
+
+    public function setTirantdeau(float $tirantdeau): static
+    {
+        $this->tirantdeau = $tirantdeau;
+
+        return $this;
+    }
+
+    public function getTypeNavire(): ?AisShipType
+    {
+        return $this->typeNavire;
+    }
+
+    public function setTypeNavire(?AisShipType $typeNavire): static
+    {
+        $this->typeNavire = $typeNavire;
+
+        return $this;
+    }
+
+    public function getPavillon(): ?Pays
+    {
+        return $this->pavillon;
+    }
+
+    public function setPavillon(?Pays $pavillon): static
+    {
+        $this->pavillon = $pavillon;
+
+        return $this;
+    }
+
+    public function getDestination(): ?Port
+    {
+        return $this->destination;
+    }
+
+    public function setDestination(?Port $destination): static
+    {
+        $this->destination = $destination;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Escale>
+     */
+    public function getEscales(): Collection
+    {
+        return $this->escales;
+    }
+
+    public function addEscale(Escale $escale): static
+    {
+        if (!$this->escales->contains($escale)) {
+            $this->escales->add($escale);
+            $escale->setHistoriqueNavire($this);
+        }
+
+        return $this;
+    }
+
+    public function removeEscale(Escale $escale): static
+    {
+        if ($this->escales->removeElement($escale)) {
+            // set the owning side to null (unless already changed)
+            if ($escale->getHistoriqueNavire() === $this) {
+                $escale->setHistoriqueNavire(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ src/Entity/Port.php
@@ -0,0 +1,174 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PortRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PortRepository::class)]
+#[Assert\Unique(fields:['indicatifs'])]
+#[ORM\Index(name:'ind_INDICATIF', columns:['indicatif'])]
+class Port
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column (name:'id')]
+    private ?int $id = null;
+
+    #[ORM\Column(name: 'nom',length: 70)]
+    private ?string $nom = null;
+
+    #[ORM\Column(name:'indicatif',length: 5)]
+    #[ORM\Regex(pattern:"/[A-Z]{5}/", massage:"L'indicatif Port a strictement 5 caracteres")]
+    private ?string $Indicatif = null;
+
+    #[ORM\ManyToMany(targetEntity: AisShiptype::class, inversedBy: 'portsCompatibles')]
+    #[ORM\JoinTable(name:'porttypecompatible')]
+    #[ORM\JoinColumn(name:'idport', referencedColumnName:'id')]
+    #[ORM\InverseJoinColumn(name:'idaisshiptype', referencedColumnName:'id')]
+    private Collection $types;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(name:'idpays',nullable: false)]
+    private ?Pays $pays = null;
+
+    #[ORM\OneToMany(mappedBy: 'destination', targetEntity: Navire::class)]
+    private Collection $navires;
+
+    #[ORM\OneToMany(mappedBy: 'port', targetEntity: Escale::class, orphanRemoval: true)]
+    private Collection $escales;
+
+    public function __construct()
+    {
+        $this->types = new ArrayCollection();
+        $this->navires = new ArrayCollection();
+        $this->escales = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getIndicatif(): ?string
+    {
+        return $this->Indicatif;
+    }
+
+    public function setIndicatif(string $Indicatif): static
+    {
+        $this->Indicatif = $Indicatif;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, AisShiptype>
+     */
+    public function getTypes(): Collection
+    {
+        return $this->types;
+    }
+
+    public function addType(AisShiptype $type): static
+    {
+        if (!$this->types->contains($type)) {
+            $this->types->add($type);
+        }
+
+        return $this;
+    }
+
+    public function removeType(AisShiptype $type): static
+    {
+        $this->types->removeElement($type);
+
+        return $this;
+    }
+
+    public function getPays(): ?Pays
+    {
+        return $this->pays;
+    }
+
+    public function setPays(?Pays $pays): static
+    {
+        $this->pays = $pays;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Navire>
+     */
+    public function getNavires(): Collection
+    {
+        return $this->navires;
+    }
+
+    public function addNavire(Navire $navire): static
+    {
+        if (!$this->navires->contains($navire)) {
+            $this->navires->add($navire);
+            $navire->setDestination($this);
+        }
+
+        return $this;
+    }
+
+    public function removeNavire(Navire $navire): static
+    {
+        if ($this->navires->removeElement($navire)) {
+            // set the owning side to null (unless already changed)
+            if ($navire->getDestination() === $this) {
+                $navire->setDestination(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Escale>
+     */
+    public function getEscales(): Collection
+    {
+        return $this->escales;
+    }
+
+    public function addEscale(Escale $escale): static
+    {
+        if (!$this->escales->contains($escale)) {
+            $this->escales->add($escale);
+            $escale->setHistoriquePort($this);
+        }
+
+        return $this;
+    }
+
+    public function removeEscale(Escale $escale): static
+    {
+        if ($this->escales->removeElement($escale)) {
+            // set the owning side to null (unless already changed)
+            if ($escale->getHistoriquePort() === $this) {
+                $escale->setHistoriquePort(null);
+            }
+        }
+
+        return $this;
+    }
+}
