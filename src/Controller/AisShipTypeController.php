9b0ea4f42f816dfd89b0e7020ed24910165df42c
MatthisDoney <matthis.doney@outlook.fr>
2024-01-08 10:54:56 +0100

    user story 1 fini, passage user story 2

--- src/Controller/AisShipTypeController.php
+++ src/Controller/AisShipTypeController.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use \App\Repository\AisShipTypeRepository;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+
 
 #[Route('/aisshiptype', name: 'aisshiptype')]
 class AisShipTypeController extends AbstractController {
@@ -21,10 +21,10 @@ class AisShipTypeController extends AbstractController {
         
     }
     
-    #[Route('/portscompatibles', name: 'portscompatibles')]
+    #[Route('/portscompatibles', name: ' portscompatibles')]
     public function portsCompatibles(Request $request, AisShipTypeRepository $repo) : Response  {
         $aisShipType = $repo->find($request->get('id'));
-        return $this->render('aisshiptype/portscompatibles.html.twig'), [
+        return $this->render('aisshiptype/portscompatibles.html.twig', [
             'aisShipType' => $aisShipType,
             ]);
         
